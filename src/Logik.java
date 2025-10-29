ea0b85ad033bd32f9eb2be0105403b236f063da3
EmilDrougge <svenemildrougge@gmail.com>
2025-10-29 19:18:17 +0100

    Adda logik klass med rough outline

---
+++ src/Logik.java
@@ -0,0 +1,30 @@
+public class Logik {
+
+    //Metod för att kolla om knappen kan flytta på sig.
+    /*
+    Kolla om knappen man klickar på är bredvid 0an
+     */
+
+
+    //Metod för att flytta knapp om det går.
+    /*
+    Om metoden som kollar om knappen är bredvid 0an så kan knappen flytta på sig.
+    Då ska denna metod flippa deras plats i arrayen.
+     */
+
+
+
+    //Metod som randomizar brädet.
+    /*Startar upp spel med 2D array som är rätt.
+     1, 2, 3, 4
+     5, 6, 7, 8
+     9, 10, 11, 12
+     13, 14, 15, 0
+     Och sen kastar runt nummrena, behövde inte faktiskt vara lösbart enlig uppgift.
+     */
+
+
+    //Metod som kollar om nuvarande bräde matchar lösningen.
+    //Spara en array som är rätt och jämför efter varje flytt kanske?
+
+}
