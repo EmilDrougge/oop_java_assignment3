fc12d548718c5ed1a3a8d3bcf7390c84ee723656
IsacAstr <strowe1000@gmail.com>
2025-10-30 11:22:20 +0100

    Added buttons and panels, working on design

--- src/Spel.java
+++ src/Spel.java
@@ -11,18 +11,32 @@ public class Spel extends JFrame implements ActionListener {
 
     JFrame jf = new JFrame("15-Spel");
 
-
+    JButton jb0 = new JButton();
     JButton jb1 = new JButton();
     JButton jb2 = new JButton();
     JButton jb3 = new JButton();
     JButton jb4 = new JButton();
     JButton jb5 = new JButton();
     JButton jb6 = new JButton();
+    JButton jb7 = new JButton();
+    JButton jb8 = new JButton();
+    JButton jb9 = new JButton();
+    JButton jb10 = new JButton();
+    JButton jb11 = new JButton();
+    JButton jb12 = new JButton();
+    JButton jb13 = new JButton();
+    JButton jb14 = new JButton();
+    JButton jb15 = new JButton();
+    JButton newGame = new JButton();
+    JPanel southPanel = new JPanel();
+    JPanel centerPanel = new JPanel();
+    JPanel northPanel = new JPanel();
+    JButton banner = new JButton();
 
 
     public Spel() {
-        jb1.addActionListener(this);
-        jf.add(gameGrid());
+        jb0.addActionListener(this);
+        jf.add(gameGridLayout());
         jf.pack();
         jf.setVisible(true);
         jf.setLocationRelativeTo(null);
@@ -31,21 +45,39 @@ public class Spel extends JFrame implements ActionListener {
     }
 
 
-    JPanel gameGrid() {
-        JPanel jp = new JPanel(new GridLayout(4,4));
-        jp.setPreferredSize(new Dimension(400, 400));
-        jp.add(jb1);
-        jp.add(jb2);
-        jp.add(jb3);
-        jp.add(jb4);
-        jp.add(jb5);
-        jp.add(jb6);
-        return jp;
+
+    JPanel gameGridLayout() {
+        centerPanel.setLayout(new GridLayout(4, 4));
+        southPanel.setLayout(new FlowLayout());
+        centerPanel.setPreferredSize(new Dimension(400, 400));
+        northPanel.setLayout(new FlowLayout());
+        centerPanel.add(jb0);
+        centerPanel.add(jb1);
+        centerPanel.add(jb2);
+        centerPanel.add(jb3);
+        centerPanel.add(jb4);
+        centerPanel.add(jb5);
+        centerPanel.add(jb6);
+        centerPanel.add(jb7);
+        centerPanel.add(jb8);
+        centerPanel.add(jb9);
+        centerPanel.add(jb10);
+        centerPanel.add(jb11);
+        centerPanel.add(jb12);
+        centerPanel.add(jb13);
+        centerPanel.add(jb14);
+        centerPanel.add(jb15);
+        southPanel.add(newGame);
+        northPanel.add(banner);
+
+        return centerPanel;
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == jb1) {
+        if (e.getSource() == jb0) {
             ljud.playSound();
         }
     }
