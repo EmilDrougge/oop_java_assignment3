2c4f4a9b0f8dda43d64b2037ffcfd820d3e48918
EmilDrougge <svenemildrougge@gmail.com>
2025-10-30 12:45:42 +0100

    Added findEmptySpace in array method and changed main to test it out.

--- src/Logik.java
+++ src/Logik.java
@@ -2,6 +2,11 @@ import java.util.Random;
 
 public class Logik {
 
+
+    boolean isNextToEmpty(int row, int col) {
+
+        return true;
+    }
     //Metod för att kolla om knappen kan flytta på sig.
     /*
     Kolla om knappen man klickar på är bredvid 0an
@@ -15,9 +20,21 @@ public class Logik {
      */
 
 
-    //Metod som randomizar brädet.
+    //Hittar 0an i en array
+    void findEmptySpace(int[][] arrayToSearch) {
 
-    //Array som är rätt by default
+        for (int i = arrayToSearch.length - 1; i >= 0; i--) {
+            for (int j = arrayToSearch[i].length - 1; j >= 0; j--) {
+
+                if (arrayToSearch[i][j] == 0) {
+                    System.out.println("Hittat 0'an på arrayplats " + i + " " + j);
+                }
+
+            }
+        }
+    }
+
+    //Array
     public int[][] gameGridArray = {
             {1, 2, 3, 4,},
             {5, 6, 7, 8,},
@@ -26,20 +43,23 @@ public class Logik {
     };
 
     //Tar array och kastar om
-    void randomizeArray(int[][] gameGridArray) {
+    void randomizeArray(int[][] arrayToRandomize) {
         Random rand = new Random();
 
-        for (int i = gameGridArray.length - 1; i > 0; i--) {
-            for (int j = gameGridArray[i].length - 1; j > 0; j--) {
+        for (int i = arrayToRandomize.length - 1; i > 0; i--) {
+            for (int j = arrayToRandomize[i].length - 1; j > 0; j--) {
+
                 int m = rand.nextInt(i + 1);
                 int n = rand.nextInt(j + 1);
 
-                int temp = gameGridArray[i][j];
-                gameGridArray[i][j] = gameGridArray[m][n];
-                gameGridArray[m][n] = temp;
+                int temp = arrayToRandomize[i][j];
+                arrayToRandomize[i][j] = arrayToRandomize[m][n];
+                arrayToRandomize[m][n] = temp;
             }
         }
     }
+
+
 
 
     //Metod som kollar om nuvarande bräde matchar lösningen.
